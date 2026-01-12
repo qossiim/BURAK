51279ace6a99c6b752b8d0c0f21514fd902f5bb4
qossiim <qosimtoirov2000@gmail.com>
2026-01-12 23:02:34 +0500

    feat: Initial Setup

---
+++ src/server.ts
@@ -0,0 +1,8 @@
+console.log("EXUCUTED!");
+
+import moment from "moment";
+const currentTime = moment().format("YYY MM DD");
+console.log(currentTime);
+
+const person: string = "Qosim";
+const count: number = 100;
